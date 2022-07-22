ec7b1e85f854949e4981500658545ce6def25547
alihatamitajik <a.hatam008@gmail.com>
2022-07-22 18:10:28 +0430

    link opencv to project

--- Edge Detector/main.cpp
+++ Edge Detector/main.cpp
@@ -7,8 +7,13 @@
 #include "edge.cuh"
 #include "cuda_runtime.h"
 
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
 
 
+using namespace cv;
 
 int main(int argc, char *argv[])
 {
